c4db76c23efe821e6607e8c63fc677734d863d94
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-02-13 21:37:59 +0530

    add new abstract method create brochure

--- src/interfaces/i_openai_operations.py
+++ src/interfaces/i_openai_operations.py
@@ -5,5 +5,11 @@ from typing import List
 class IOpenAIOperations(ABC):
 
     @abstractmethod
-    def select_relevant_links(self) -> List[str]:
+    def select_relevant_links(self, base_url: str, links: list) -> List[str]:
+        pass
+
+    @abstractmethod
+    def create_brochure(
+        self, company_name: str, contents: str, relevent_links: list
+    ) -> str:
         pass
